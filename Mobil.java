60fc7974f63dd77476d2ccafbaa32bc9f3818bfe
Rufus8702 <Jamaludinahmad013@gmail.com>
2022-10-04 14:05:00 +0700

    Sesi 2 OOP

---
+++ Mobil.java
@@ -0,0 +1,25 @@
+public class Mobil {
+    String merk;
+    int tahun_pembuatan;
+
+    public void setMerk(String merk){
+        this.merk = merk;
+    }
+
+    public void setTahunBuat(int tahun_pembuatan){
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    public String getMerk(){
+        return merk;
+    }
+
+    public int getTahunBuat(){
+        return tahun_pembuatan;
+    }
+
+    public void infoMobil(){
+        System.out.println("Merek " + getMerk());
+        System.out.println("Tahun " + getTahunBuat());
+    }
+}
---
+++ MobilMewah.java
@@ -0,0 +1,15 @@
+public class MobilMewah {
+    
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil();
+        Mobil mobil2 = new Mobil();
+        
+        mobil1.setMerk("bi em dabel yu");
+        mobil1.setTahunBuat(1999);
+
+        mobil2.setMerk("Avanza");
+        mobil2.setTahunBuat(2000);
+
+        mobil1.infoMobil();
+    }
+}
