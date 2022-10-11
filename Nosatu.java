bad466079fb29cceba7cad8d9eccf750baee43f3
Rufus8702 <Jamaludinahmad013@gmail.com>
2022-10-11 08:06:37 +0700

    menghapus kesalahan

--- Nosatu.java
+++
@@ -1,8 +0,0 @@
-public class Nosatu {
-    public static void main(String[] args) {
-        System.out.println("Belajar Java");
-        System.out.println("Sangat mudah sekali");
-        System.out.println("dan sangat menyenangkan");
-
-    }
-}
